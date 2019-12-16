5849d6ef10d9ab44eccbb9cf597e8295a245cda2
okuraofvegetable <okuraofvegetable@gmail.com>
2019-12-16 17:24:23 +0900

    error report by atsunobu

--- ._d/global_array.d
+++
@@ -1,2 +0,0 @@
-global_array.cmo : syntax.cmo global_array.cmi
-global_array.cmx : syntax.cmx global_array.cmi
--- ._d/virtual.d
+++
@@ -1,4 +0,0 @@
-virtual.cmo : type.cmo setGlobalArray.cmi m.cmo id.cmo closure.cmi asm.cmi \
-    virtual.cmi
-virtual.cmx : type.cmx setGlobalArray.cmx m.cmx id.cmx closure.cmx asm.cmx \
-    virtual.cmi
